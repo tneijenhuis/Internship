d86d84141bb882a0f555a0086343afdba513dd6d
Tim Neijenhuis <Tim-Neijenhuis@hotmail.com>
2019-12-20 12:20:12 +0100

    added script to remove ANISOU from PDB files for interfacea recognition

---
+++ scripts/del_anisou.py
@@ -0,0 +1,21 @@
+import argparse
+
+def remove_anisou(pdb):
+	with open(pdb) as file:
+		new_file = ""
+		for line in file:
+			if line[0:6] != "ANISOU":
+				new_file += line
+
+	return new_file
+
+def main():
+	parser = argparse.ArgumentParser(description="count intermolecular clashes")
+	parser.add_argument("structure", help="The path to the pdb file" )
+	args = parser.parse_args()
+	new_file = remove_anisou(args.structure)
+
+	print(new_file)
+
+if __name__ == "__main__":
+	main()
